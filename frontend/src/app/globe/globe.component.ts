6fff8fdf5fb5f4d766c9b469bccbd525f5cb6869
Sho Nihei <shonihei@gmail.com>
2019-03-23 01:49:34 -0400

    migrated to three.js

--- frontend/src/app/globe/globe.component.ts
+++ frontend/src/app/globe/globe.component.ts
@@ -1,93 +1,140 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Coordinate } from './coordinate';
+import * as THREE from "three";
+import { Raycaster, Vector2 } from 'three';
+// import * as p5 from "p5";
 
-import * as p5 from "p5";
+export class MousePosition {
+  x: number;
+  y: number;
+}
 
 @Component({
   selector: 'app-globe',
   templateUrl: './globe.component.html',
   styleUrls: ['./globe.component.sass']
 })
-export class GlobeComponent implements OnInit {
-  @ViewChild('canvas') canvas: ElementRef;
-  private sketch: p5;
-  private earthTexture: p5.Image;
+export class GlobeComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas') canvasRef: ElementRef;
+  // private sketch: p5;
+  // private earthTexture: p5.Image;
+  private renderer: THREE.WebGLRenderer;
+  private camera: THREE.PerspectiveCamera;
+  private cameraTarget: THREE.Vector3;
+  public scene: THREE.Scene;
+  private texture: THREE.Texture;
+  private globe: THREE.Group;
+  private raycaster: THREE.Raycaster;
+  private mouse: THREE.Vector2;
 
-  constructor() { }
-
-  ngOnInit() {
-    this.sketch = new p5(this.draw, this.canvas.nativeElement);
-    console.log("created a sketch");
-    console.log(this.sketch);
+  public fieldOfView: number = 45;
+  public nearClippingPane: number = 0.1;
+  public farClippingPane: number = 10000;
+  private lastPosition: MousePosition = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
   }
 
-  testEvent() {
-    console.log("received click event from p5");
-    console.log(this);
-    console.log(this.sketch);
+  constructor() {
+    this.render = this.render.bind(this);
+    this.raycaster = new Raycaster();
+    this.mouse = new Vector2();
   }
 
-  private onWindowResize() {
-    this.sketch.resizeCanvas(this.sketch.windowWidth, this.sketch.windowHeight);
+  ngOnInit() { }
+
+  ngAfterViewInit() {
+    this.createCamera();
+    this.createScene();
   }
 
-  // TODO: fix any type for p
-  private draw(p: any) {
-    p.r = 350;
-    p.coordinates = [
-      { lat: -33.8688, long: 151.2093 }, // Sydney, Australia
-      { lat: 35.6762, long: 139.6503 },  // Tokyo, Japan
-      { lat: 40.7128, long: -74.0060 },  // New York, USA
-      { lat: 26.8206, long: 30.8025 }    // Somewhere, Egypt
-    ];
+  public onClick(e: MouseEvent) {
+    this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+    console.log(this.mouse);
+  }
 
-    p.preload = () => {
-      p.img = p.loadImage('../assets/img/earthDark.jpg');
+  private get canvas(): HTMLCanvasElement {
+    return this.canvasRef.nativeElement;
+  }
+
+  private createScene() {
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x000000);
+
+    const RADIUS = 200;
+    const SEGMENTS = 50;
+    const RINGS = 50;
+    this.globe = new THREE.Group();
+    this.scene.add(this.globe);
+    var loader = new THREE.TextureLoader();
+    var self = this;
+    loader.load('../assets/img/earthDark.jpg', function (texture) {
+      console.log("success");
+      var sphere = new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS);
+      var material = new THREE.MeshBasicMaterial({ map: texture });
+      var mesh = new THREE.Mesh(sphere, material);
+      self.globe.add(mesh);
+      self.globe.position.z = -300;
+
+      self.camera.lookAt(self.globe.position);
+      self.startRendering();
+    });
+  }
+
+  private createCamera() {
+    let aspectRatio = this.getAspectRatio();
+    this.camera = new THREE.PerspectiveCamera(
+      this.fieldOfView,
+      aspectRatio,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+
+    // Set position and look at
+    this.camera.position.x = 0;
+    this.camera.position.y = 0;
+    this.camera.position.z = 500;
+  }
+
+  private getAspectRatio(): number {
+    let height = this.canvas.clientHeight;
+    if (height === 0) {
+      return 0;
     }
+    return this.canvas.clientWidth / this.canvas.clientHeight;
+  }
 
-    p.setup = () => {
-      const cnv = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
-      // work around needed to remove weird 30px offset
-      cnv.canvas.style.display = 'block';
-    }
+  private startRendering() {
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      antialias: true
+    });
+    this.renderer.setPixelRatio(devicePixelRatio);
+    this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
-    p.hello = true;
-    p.draw = () => {
-      p.background(0);
-      p.orbitControl(1, 0);
-      // p.camera(0, 0, 600, 0, 0, 0, 0, 1, 0);
-      // p.translate(0, 0, 250);
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer.setClearColor(0xffffff, 1);
+    this.renderer.autoClear = true;
 
-      p.rotateY(p.frameCount * -0.001);
-      p.noStroke();
-      p.texture(p.img);
-      p.sphere(350);
+    this.render();
+  }
 
-      p.drawCoordinates();
-    }
+  private test = true;
 
-    // p.mousePressed = () => {
-    //   console.log(p.mouseX, p.mouseY);
-    // }
-
-    p.drawCoordinates = () => {
-      for (let coordinate of p.coordinates) {
-        const lat = p.radians(coordinate.lat);
-        const long = p.radians(coordinate.long);
-
-        //convert to cartesian coordinates
-        var x = p.r * Math.cos(lat) * Math.sin(long + p.radians(180));
-        var y = p.r * Math.sin(-lat);
-        var z = p.r * Math.cos(lat) * Math.cos(long + p.radians(180));
-
-        // draw coordiante
-        p.push();
-        p.translate(x, y, z);
-        p.fill("red");
-        p.sphere(5, 5, 5);
-        p.pop();
+  public render() {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObjects(this.scene.children, true);
+    if (this.test) {
+      this.test = false;
+      for (let object of intersects) {
+        console.log(object);
       }
     }
-  }
 
+    requestAnimationFrame(this.render);
+    this.globe.rotation.y -= 0.005;
+    this.renderer.render(this.scene, this.camera);
+  }
 }
