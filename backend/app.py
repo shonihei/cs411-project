bba484ac48ed1a8feae81c947483a63af06e461b
Sho Nihei <shonihei@gmail.com>
2019-04-17 12:46:31 -0400

    added an endpoint to retrieve random articles

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, Response, json, request
+from flask import Flask, Response, request
 from flask_cors import CORS
 from newsapi import NewsApiClient
 from dotenv import load_dotenv
 import os
+import pymongo
+from bson.json_util import dumps
 import atexit
 from apscheduler.schedulers.background import BackgroundScheduler
 from backgroundjob import extract_articles
@@ -13,6 +15,13 @@ APP_ROOT = os.path.dirname(__file__)
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 newsapi = NewsApiClient(api_key=os.getenv('NEWS_API_KEY'))
+
+# connect to db
+CONNECTION_TEMPLATE = "mongodb+srv://{username}:{password}@worldnewsapp-1zwxu.mongodb.net/test?retryWrites=true"
+url = CONNECTION_TEMPLATE.format(username=os.getenv('DB_USERNAME'), password=os.getenv('DB_PW'))
+client = pymongo.MongoClient(url)
+db = client.global_news
+article_collection = db.articles
 
 # initialize and start background job
 scheduler = BackgroundScheduler()
@@ -29,18 +38,23 @@ app = Flask(__name__)
 CORS(app)
 
 @app.route('/articles', methods=['GET'])
-def home():
-    news = newsapi.get_everything(
-        q=request.args.get('q'),
-        language='en',
-        sort_by='relevancy',
-        page=2
-    )
-    js = json.dumps(news)
-    if news['status'] == 'ok':
-        return Response(js, status=200, mimetype='application/json')
-    else:
-        return Response(js, status=500)
-
+def get_n_random_articles():
+    n = 20
+    if 'n' in request.args:
+        n = int(request.args.get('n'))
+    try:
+        articles = list(article_collection.aggregate([{'$sample': {'size': n}}]))
+        status = 200
+        payload = {
+            'articles': articles
+        }
+    except Exception:
+        status = 500
+        payload = {
+            'status': 'failed'
+        }
+    js = dumps(payload)
+    return Response(js, status=status, mimetype='application/json')
+    
 if __name__ == '__main__':
     app.run(use_reloader=False)
